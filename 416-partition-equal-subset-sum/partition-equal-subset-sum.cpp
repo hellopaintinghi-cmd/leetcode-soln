4fcac5754293e99779ff245f19bf04bd4d9ad170
priyanshi <hellopaintinghi@gmail.com>
2026-03-07 00:16:50 +0530

    Time: 75 ms (82.70%) | Memory: 13.5 MB (86.30%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 != 0)
+            return false;
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int s = target; s >= nums[i]; s--) {
+                dp[s] = dp[s] || dp[s - nums[i]];
+            }
+        }
+        return dp[target];
+    }
+};
