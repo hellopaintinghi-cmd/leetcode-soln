32c0344f6344ab7879d8cfbfd52915e5f96e9c6e
priyanshi <hellopaintinghi@gmail.com>
2026-05-26 16:56:01 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (99.79%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -6,10 +6,10 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i] == 1){
                 curr_max++;
-                max_count = max(curr_max, max_count);
             }else{
                 curr_max = 0;
             }
+            max_count = max(curr_max, max_count);
         }
         return max_count;
     }
