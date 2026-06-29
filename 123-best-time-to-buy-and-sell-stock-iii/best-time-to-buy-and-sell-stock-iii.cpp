fd353e6a771fb4dd51de1b9072011ee7f649e374
priyanshi <hellopaintinghi@gmail.com>
2026-06-29 11:50:37 +0530

    Time: 448 ms (26.30%) | Memory: 225.5 MB (12.34%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(int i, int buy, int cap, vector<int>& prices,
+              vector<vector<vector<int>>>& dp) {
+
+        if (i == prices.size() || cap == 0)
+            return 0;
+
+        if (dp[i][buy][cap] != -1)
+            return dp[i][buy][cap];
+
+        if (buy) {
+            return dp[i][buy][cap] = max(
+                -prices[i] + solve(i + 1, 0, cap, prices, dp),
+                solve(i + 1, 1, cap, prices, dp)
+            );
+        }
+
+        return dp[i][buy][cap] = max(
+            prices[i] + solve(i + 1, 1, cap - 1, prices, dp),
+            solve(i + 1, 0, cap, prices, dp)
+        );
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(2, vector<int>(3, -1))
+        );
+
+        return solve(0, 1, 2, prices, dp);
+    }
+};
