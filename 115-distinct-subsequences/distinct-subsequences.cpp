48e1f93b0d370570751a4422fd03bdeff980206c
priyanshi <hellopaintinghi@gmail.com>
2026-07-04 11:47:01 +0530

    Time: 27 ms (69.35%) | Memory: 44.4 MB (21.80%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,38 +1,46 @@
 class Solution {
 public:
-    int solve(string s, string t, int i, int j, vector<vector<long long>>& dp) {
-        if (j == 0)
+    int solve(int i, int j, string s, string t, vector<vector<int>>& dp) {
+        if (j < 0) {
             return 1;
-        if (i == 0)
+        }
+        if (i < 0) {
             return 0;
-
+        }
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
-
-        if (s[i - 1] == t[j - 1]) {
-            return dp[i][j] = (solve(s, t, i - 1, j - 1, dp) +
-                               solve(s, t, i - 1, j, dp));
+        if (s[i] == t[j]) {
+            dp[i][j] =
+                solve(i - 1, j, s, t, dp) + solve(i - 1, j - 1, s, t, dp);
         } else {
-            return dp[i][j] = solve(s, t, i - 1, j, dp);
+            dp[i][j] = solve(i - 1, j, s, t, dp);
         }
+        return dp[i][j];
     }
 
-    long long numDistinct(string s, string t) {
+    int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<unsigned long long> prev(m+1, 0), curr(m+1, 0);
-        prev[0] = curr[0] = 1;
+
+        vector<vector<unsigned long long>> dp(
+            n + 1, vector<unsigned long long>(m + 1, 0));
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= min(i, m); j++) {
+            for (int j = 1; j <= m; j++) {
+
                 if (s[i - 1] == t[j - 1]) {
-                    curr[j] = prev[j - 1] + prev[j];
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
                 } else {
-                    curr[j] = prev[j];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
-            prev = curr;
         }
-        return prev[m];
+
+        return (int)dp[n][m];
     }
 };
