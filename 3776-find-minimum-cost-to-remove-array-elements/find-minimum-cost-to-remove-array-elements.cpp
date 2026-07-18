c6b7cbda701e26c48f51c754daff18e680b97470
priyanshi <hellopaintinghi@gmail.com>
2026-07-18 12:35:02 +0530

    Time: 589 ms (10.86%) | Memory: 182.3 MB (57.94%) - LeetSync

--- 3776-find-minimum-cost-to-remove-array-elements/find-minimum-cost-to-remove-array-elements.cpp
+++ 3776-find-minimum-cost-to-remove-array-elements/find-minimum-cost-to-remove-array-elements.cpp
@@ -21,7 +21,23 @@ public:
     }
     int minCost(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int> (n, -1));
-        return solve(nums, 0, 1, dp);
+        vector<vector<int>> dp(n, vector<int>(n + 2, 0));
+        for(int i = 0;i<n;i++){
+            dp[i][n] = nums[i];
+        }
+        for(int i = 0;i < n-1;i++){
+            dp[i][n-1] = max(nums[i], nums[n-1]);
+        }
+
+        for(int j = n-2;j>=1;j--){
+            for(int i = n-2;i >= 0;i--){
+                dp[i][j] = min({
+                    max(nums[i], nums[j]) + dp[j + 1][j + 2],
+                    max(nums[i], nums[j + 1]) + dp[j][j + 2],
+                    max(nums[j], nums[j + 1]) + dp[i][j + 2]
+                });
+            }
+        }
+        return dp[0][1];
     }
 };
