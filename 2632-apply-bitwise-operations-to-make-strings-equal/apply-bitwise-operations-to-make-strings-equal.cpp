eb05a44ecf8162d502c252ed5ca23039d3f31be4
priyanshi <hellopaintinghi@gmail.com>
2026-07-22 15:19:05 +0530

    Time: 5 ms (16.20%) | Memory: 15.9 MB (21.13%) - LeetSync

---
+++ 2632-apply-bitwise-operations-to-make-strings-equal/apply-bitwise-operations-to-make-strings-equal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool makeStringsEqual(string s, string target) {
+        int n = s.size();
+        int cnt1a = 0;
+        int cnt1b = 0;
+        for(int i = 0;i < n; i++){
+            if(s[i] == '1'){
+                cnt1a++;
+            }
+        }
+        for(int i = 0;i < n; i++){
+            if(target[i] == '1'){
+                cnt1b++;
+            }
+        }
+        return (cnt1a > 0) == (cnt1b > 0);
+    }
+};
