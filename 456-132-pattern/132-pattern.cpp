c5439a6a7cb9bf6b31baad9bf94e2bde6576fbaf
priyanshi <hellopaintinghi@gmail.com>
2026-07-26 22:47:58 +0530

    Time: 15 ms (26.93%) | Memory: 70.7 MB (46.73%) - LeetSync

--- 456-132-pattern/132-pattern.cpp
+++ 456-132-pattern/132-pattern.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         int n = nums.size();
+        if (n<3) return 0;
         stack<int> st;
         int third = INT_MIN;
         for(int i = n-1; i >= 0; i--){
