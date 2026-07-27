b052460a6db103b845ee0beebaead7471834c7e5
priyanshi <hellopaintinghi@gmail.com>
2026-07-27 13:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (85.48%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -2,10 +2,26 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int opt1 = (nums[0] - 1) * (nums[1] - 1);
-        int opt2 = (nums[0] - 1) * (nums[n - 1] - 1);
-        int opt3 = (nums[n-1] - 1) * (nums[n-2] - 1);
-        return max({opt1, opt2, opt3});
+        int max1 = INT_MIN, max2 = INT_MIN;
+        int min1 = INT_MAX, min2 = INT_MAX;
+        for(int x : nums){
+            if(x >= max1){
+                max2 = max1;
+                max1 = x;
+            }else if(x > max2){
+                max2 = x;
+            }
+            if(x <= min1){
+                min2 = min1;
+                min1 = x;
+            }else if(x < min2){
+                min2 = x;
+            }
+        }
+        long long opt1 = 1LL * (max1 - 1) * (max2 - 1);
+        long long opt2 = 1LL * (max1 - 1) * (min1 - 1);
+        long long opt3 = 1LL * (min2 - 1) * (min1 - 1);
+
+        return (int)max({opt1, opt2, opt3});
     }
 };
