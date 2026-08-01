9cc19a42f8322a48f0b9d0b4b51fe1059c681be1
priyanshi <hellopaintinghi@gmail.com>
2026-08-01 13:26:07 +0530

    Time: 31 ms (54.31%) | Memory: 17.4 MB (66.96%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> dp(n, 1);
         vector<int> count(n, 1);
 
-        for(int i = 0; i < n ;i++){
+        for(int i = 1; i < n ;i++){
             for(int j = 0; j<i;j++){
                 if(nums[j] < nums[i]){
                     if(dp[i] == dp[j] + 1){
