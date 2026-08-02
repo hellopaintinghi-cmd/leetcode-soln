2f1b5d883f4783266db9eef5732fd8bec58f7805
priyanshi <hellopaintinghi@gmail.com>
2026-08-02 11:44:54 +0530

    Time: 31 ms (30.57%) | Memory: 139.3 MB (46.70%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,16 +4,17 @@ public:
         int n = nums.size();
         deque<int> dq;
         vector<int> ans;
-        for(int i=0;i<n;i++){
+        
+        for(int i = 0; i < n ; i++){
             while(!dq.empty() && dq.front() <= i - k){
                 dq.pop_front();
             }
-            while(!dq.empty() && nums[dq.back()] <= nums[i]){
+            while(!dq.empty() && nums[dq.back()] < nums[i]){
                 dq.pop_back();
             }
             dq.push_back(i);
 
-            if(i >= k-1){
+            if(i >= k - 1){
                 ans.push_back(nums[dq.front()]);
             }
         }
