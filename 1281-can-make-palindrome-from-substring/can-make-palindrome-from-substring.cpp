714e40506c564aacc193c3f70071731c2ea17bef
priyanshi <hellopaintinghi@gmail.com>
2026-09-09 22:09:53 +0530

    Time: 4 ms (90.30%) | Memory: 98 MB (87.34%) - LeetSync

---
+++ 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        int n = s.size();
+        vector<int> pref(n + 1, 0);
+        for(int i = 0; i < n; i++){
+            pref[i + 1] = pref[i] ^ (1 << (s[i] - 'a'));
+        }
+
+        vector<bool> ans;
+        for(auto &q : queries){
+            int l = q[0];
+            int r = q[1];
+            int k = q[2];
+
+            int mask = pref[r + 1] ^ pref[l];
+            int oddcnt = __builtin_popcount(mask);
+
+            ans.push_back(oddcnt/2 <= k);
+        }
+
+        return ans;
+    }
+};
